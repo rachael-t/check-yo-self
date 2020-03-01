20a6e609c503c74b94e78209d362e961b18c5bd7
Rachael Thomas <rnthomas001@gmail.com>
2020-03-01 14:03:35 -0700

    Edit parameters and crate saveToStorage method

--- todo-list.js
+++ todo-list.js
@@ -1,13 +1,15 @@
 class ToDoList {
-  constructor(uniqueID, title) {
+  constructor(uniqueID, title, tasks) {
     this.id = uniqueID;
     this.title = title;
+    this.tasks = tasks;
     this.urgent = false;
-    this.tasks = [];
   }
-  saveToStorage() {
+  saveToStorage(toDoList) {
+    var stringifiedToDoList = JSON.stringify(toDoList)
+    localStorage.setItem('toDos', stringifiedToDoList);
+  }
 
-  }
   deleteFromStorage() {
 
   }
